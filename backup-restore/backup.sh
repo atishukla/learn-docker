16796f2ddeca2cd2bb764f356489c0499b74d18b
atishukla <atishayshukla@gmail.com>
2020-07-25 23:29:06 +0200

    fixed quotes for string comparison

--- backup-restore/backup.sh
+++ backup-restore/backup.sh
@@ -34,7 +34,7 @@ do
     DVNAME=$(echo $nameetdest | awk '{print $1}')
     DVDEST=$(echo $nameetdest | awk '{print $2}')
     # For the ones which are not data the name will be null
-    if [ $DVNAME -ne 'null' ]
+    if [ "$DVNAME" -ne 'null' ]
     then
       echo $DVDEST
     fi
