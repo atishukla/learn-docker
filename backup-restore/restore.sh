7149c37768388df9d20cd15c2fbeb600d724aa78
atishukla <atishayshukla@gmail.com>
2020-07-26 17:59:37 +0200

    check for valid date

--- backup-restore/restore.sh
+++ backup-restore/restore.sh
@@ -5,9 +5,16 @@
 
 if [ $# -ne 1 ]
 then
-  echo 'Run the script as restore.sh <YYYYMMDD> which is the backup date to restore to..'
+  echo 'Run the script as restore.sh with latest or date <YYYYMMDD> which is the backup date to restore to..'
+  echo 'Usage: restore.sh <latest|20200725>'
   exit 1
 else
-  TIMESTAMP=$(date --date="$1" +%Y%m%d)
-  echo "The requested timestamp is $TIMESTAMP..."
+  if [ "$1" == 'latest' ]
+  then
+    TIMESTAMP=$(date +%Y%m%d)
+  else
+    if [ "${#1}" -lt 8 ]; then echo "invalid date ..should have MM & dd"; exit 1; fi
+    TIMESTAMP=$(date --date="$1" +%Y%m%d)
+  fi
 fi
+echo "Target timestamp is **** $TIMESTAMP ****"
